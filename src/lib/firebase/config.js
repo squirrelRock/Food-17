0a9018c43e53504dbffc9f5f58627b70e86e9a5d
Kelly Durkin <kdurkin@bearcubs.santarosa.edu>
2024-10-11 12:46:06 -0700

    add import statements to config.js

--- src/lib/firebase/config.js
+++ src/lib/firebase/config.js
@@ -1,8 +1,3 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
-
 const config = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
